ee541678d44e424368cd4ef4dc7c6b68982ca051
davisbalbino <davidossantosbalbinomarcelino@gmail.com>
2024-03-19 19:48:26 -0300

    Configurado o script para mandar o email para o meu pessoal

---
+++ scripts/shell.sh
@@ -0,0 +1,8 @@
+ls
+cd Aula-GitHub-Actions
+ls
+echo "AAAHAHAHAAA"
+sudo apt-get install mailutils
+echo "Fim da instalacao"
+echo "Mandando e-mail com mail do linux" | mail -s "a subject" davidossantosbalbinomarcelino@gmail.com
+# posso fazer o que eu quiser aqui
