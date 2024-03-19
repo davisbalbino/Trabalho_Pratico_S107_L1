133efada5d203e56627c5bdce4b9303d1833b51f
davisbalbino <davidossantosbalbinomarcelino@gmail.com>
2024-03-18 21:51:08 -0300

    Finalizadas as classes do SRC MAIN

---
+++ inatel-pipeline/src/main/java/inatel/CupomDesconto.java
@@ -0,0 +1,17 @@
+package inatel;
+public class CupomDesconto {
+
+    private double desconto;
+
+    public CupomDesconto(double desconto) {
+        this.desconto = desconto;
+    }
+
+    public double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(double desconto) {
+        this.desconto = desconto;
+    }
+}
