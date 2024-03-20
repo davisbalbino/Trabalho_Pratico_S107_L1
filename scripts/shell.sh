f5f09c34c584b4d93222975eaf4bc7d7d13d527c
davisbalbino <davidossantosbalbinomarcelino@gmail.com>
2024-03-20 19:49:57 -0300

    alterações no shell

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,4 +1,4 @@
 email_dest="$EMAIL_DESTINATION"
 sudo apt-get install mailutils
 echo "Fim da instalacao"
-echo "Mandando e-mail com mail do linux" | mail -s "a subject" $email_dest
+echo "Mandando e-mail com mail do linux" | mail -s "a subject" davidossantosbalbinomarcelino@gmail.com
